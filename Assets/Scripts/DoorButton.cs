85faa8ae2b0d69c196a6024f505ca866620a6d74
David <kdavidk98@gmail.com>
2019-11-02 01:48:43 -0300

    Button interaction

---
+++ Assets/Scripts/DoorButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour
+{
+    public void Press()
+    {
+        Debug.Log("Botao apertado");
+    }
+}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteract : MonoBehaviour
+{
+    private GameObject raycastObj;
+
+    [SerializeField] private int rayLength = 10;
+    [SerializeField] private LayerMask layerMaskInteract;
+    public GameObject interactText;
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, out hit, rayLength, layerMaskInteract.value))
+        {
+            if (hit.collider.CompareTag("Button"))
+            {
+                raycastObj = hit.collider.gameObject;
+
+                interactText.SetActive(true);
+
+                if (Input.GetKeyDown("e"))
+                {
+                    Debug.Log("INteracao");
+                    raycastObj.SendMessage("Press");
+                }
+            }
+            else
+            {
+                interactText.SetActive(false);
+            }
+        }
+        else
+        {
+            interactText.SetActive(false);
+        }
+    }
+
+}
