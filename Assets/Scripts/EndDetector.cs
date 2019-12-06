55b48578a916336ef64439968a51965a01f4c1b6
David Mees Knijnik <kdavidk98@gmail.com>
2019-12-06 16:03:54 -0300

    Cronômetro

---
+++ Assets/Scripts/EndDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDetector : MonoBehaviour
+{
+
+    public GameObject EndMenu;
+    public LevelTimer Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EndMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            EndMenu.SetActive(true);
+            Timer.StopTimer();
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour
+{
+
+    public Text text;
+    public float theTime = 0;
+    public float speed = 1;
+    private bool playing = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playing)
+        {
+            theTime += Time.deltaTime * speed;
+            string hours = Mathf.Floor((theTime % 216000) / 3600).ToString("00");
+            string minutes = Mathf.Floor((theTime % 3600) / 60).ToString("00");
+            string seconds = (theTime % 60).ToString("00");
+            text.text = hours + ":" + minutes + ":" + seconds;
+        }
+       
+    }
+
+    public void StopTimer()
+    {
+        playing = false;
+    }
+
+    public void StartTimer()
+    {
+        playing = true;
+    }
+
+}
+
