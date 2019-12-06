d236c3da13155060eef41e1545cd2bb6c3d79c37
David <kdavidk98@gmail.com>
2019-12-06 18:20:01 -0300

    Texto inicial

---
+++ Assets/Scripts/StartForm.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartForm : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject startUI;
+
+    void Start()
+    {
+        Pause();
+    }
+
+    public void Resume()
+    {
+        startUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Pause()
+    {
+        startUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+}
